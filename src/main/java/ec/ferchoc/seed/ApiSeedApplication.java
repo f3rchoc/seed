1d8f4d84dce8a94044a3144c7d9d7fd240fb77ae
Wilmer Caiza <wilmercaiza@gmail.com>
2020-12-28 22:52:18 -0500

    Config seed api rest project

---
+++ src/main/java/ec/ferchoc/seed/ApiSeedApplication.java
@@ -0,0 +1,13 @@
+package ec.ferchoc.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/ferchoc/seed/ApiSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.ferchoc.seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
